6f8e1257d0a666e4e7cc50132121fc6cbd2ca4aa
Hanna Ismahilova <hannaismahilova@gmail.com>
2019-04-24 18:57:07 +0200

    laptopBagsText name changes

--- ITCrowdProject/src/main/java/pl/infoshare/tests/FooterTest.java
+++ ITCrowdProject/src/main/java/pl/infoshare/tests/FooterTest.java
@@ -67,7 +67,7 @@ public class FooterTest {
         footerPage.clickLaptopBagsButtonOnFuter();
 
         LaptopBagsCataloguePage laptopBagsCataloguePage = new LaptopBagsCataloguePage(driver);
-        assertThat(laptopBagsCataloguePage.laptopBagTekst()).isEqualTo("Laptop bags");
+        assertThat(laptopBagsCataloguePage.laptopBagText()).isEqualTo("Laptop bags");
 
 
     }
