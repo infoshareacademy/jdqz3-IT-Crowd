73e3335aaa3b040399e2c116801b3049f929a50a
Hanna-Ismahilova <hi.infoshare@gmail.com>
2019-05-17 13:21:11 +0200

    final version

--- ITCrowdProject/src/main/java/pl/infoshare/test/ShopizerTestSuite.java
+++ ITCrowdProject/src/main/java/pl/infoshare/test/ShopizerTestSuite.java
@@ -8,7 +8,7 @@ import pl.infoshare.pages.MainPage;
 import pl.infoshare.tests.*;
 
 @RunWith(Categories.class)
-@Categories.IncludeCategory({PurchaseTests.class, AddToCartTest.class, AddToCartRandomBagTest.class, AddToCartNextRandomBagTest.class, PurchaseLaptopBagTest.class, HandbagsOnFooter.class, ValidateNameIsRequired.class, ValidateLastnameIsRequired.class, ValidateStreetAddressIsRequired.class, ValidateStateIsRequired.class, ValidatePostalCodeIsRequired.class, ValidateEmailAddressIsRequired.class, ValidateEmailAddressIsRequired.class, ValidatePhoneNumberIsRequired.class, BeachBagsOnFooter.class, RegisterUnderMyAccount.class})
+@Categories.IncludeCategory({PurchaseTests.class, AddToCartTest.class, AddToCartRandomBagTest.class, AddToCartNextRandomBagTest.class, PurchaseLaptopBagTest.class, HandbagsOnFooter.class, ValidateNameIsRequired.class, ValidateLastnameIsRequired.class, ValidateStreetAddressIsRequired.class, ValidateStateIsRequired.class, ValidatePostalCodeIsRequired.class, ValidateEmailAddressIsRequired.class, ValidateEmailAddressIsRequired.class, ValidatePhoneNumberIsRequired.class, BeachBagsOnFooter.class, RegisterUnderMyAccount.class, SignInUnderMyAccount.class})
 @Suite.SuiteClasses({PurchaseTest.class, FooterTest.class, BillingInformationValidationTest.class, QuantityTest.class, MyAccountTest.class})
 public class ShopizerTestSuite {
 }
