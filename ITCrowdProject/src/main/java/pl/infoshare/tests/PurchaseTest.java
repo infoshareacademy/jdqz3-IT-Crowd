e0bec7eb3bb7c3d4fb1c2dad1c5261e761ac0831
Mateusz Zieliński <zielak20051@gmail.com>
2019-05-19 10:06:15 +0200

    Change Class name

--- ITCrowdProject/src/main/java/pl/infoshare/tests/PurchaseTest.java
+++ ITCrowdProject/src/main/java/pl/infoshare/tests/PurchaseTest.java
@@ -50,9 +50,9 @@ public class PurchaseTest {
         basePage.close();
     }
 
-    @Category(PurchaseTests.class)
+    @Category(PurchaseHandBag.class)
     @Test
-    public void purchase() {
+    public void purchaseHandBag() {
         MainPage mainPage = new MainPage(driver);
         mainPage.chooseHandbagsCategory();
 
