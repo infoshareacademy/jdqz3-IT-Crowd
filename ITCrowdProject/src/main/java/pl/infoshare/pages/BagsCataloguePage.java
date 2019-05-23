548c8f758a0db455359848b6f5705740fceaa817
Zieliński Mateusz <mzielins@sevenet.local>
2019-05-23 11:49:05 +0200

    V1

---
+++ ITCrowdProject/src/main/java/pl/infoshare/pages/BagsCataloguePage.java
@@ -0,0 +1,45 @@
+package pl.infoshare.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import pl.infoshare.elements.Button;
+import pl.infoshare.elements.Label;
+import pl.infoshare.pages.sections.ShippingCart;
+
+public class BagsCataloguePage extends BasePage{
+
+
+
+    private Button addToCart;
+    private By chackoutLink;
+    private Button checkout;
+    private By bag;
+
+    private ShippingCart shippingCart;
+
+    public BagsCataloguePage(WebDriver driver){
+        super(driver);
+        this.url = "http://demo.shopizer.com:8080/shop/category/bags.html/ref=c:52";
+        this.driver.get(this.url);
+    }
+
+    public void addToCart(){
+        this.addToCart.doubleClick();
+    }
+    public void clickOnShipping() {
+        shippingCart = new ShippingCart(this.driver);
+        this.shippingCart.getShippingCart();
+    }
+
+    public void checkout(){
+        this.chackoutLink= By.cssSelector("li.checkout-bg a");
+        this.checkout= new Button(this.driver, this.chackoutLink);
+        checkout.clickWithJs();
+    }
+
+    public void getRandomBag(String name){
+        bag = By.cssSelector("#productsContainer [item-name='"+name+"'] div.store-btn-addtocart");
+        this.addToCart = new Button(this.driver, this.bag);
+    }
+
+}
